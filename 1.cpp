faaa54c42601b10a2683cde3553ec150be527882
bariqjinan <jinanbariq50@gmail.com>
2023-12-01 17:16:15 +0700

    menambahkan line 3 + 9

--- 1.cpp
+++ 1.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
     cout << " Di atas awan \n ";
     cout << " BBBBBBB\n";
     cout << "C";
     cout << "D";
+    cout << 3 + 9 << endl;
     return 0;
 }
