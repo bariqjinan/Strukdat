535bf2cd9f8507f591a18eb10413776a7873a217
bariqjinan <jinanbariq50@gmail.com>
2023-12-05 20:58:42 +0700

    Dibikin jadi pointer

--- 1.cpp
+++ 1.cpp
@@ -1,17 +1,38 @@
 #include <iostream>
 #include <windows.h>
+#include <string.h>
 #include "MMSystem.h"
+#include <winnt.h>
 
 using namespace std;
 
+struct lagu
+{
+    string judul;
+    lagu *next;
+};
+
+typedef lagu *pointer;
+
 int main()
 {
+    pointer first = new lagu;
+
+    string ext = ".wav";
+
+    cout << "Masukkan Judul Lagu: " << endl;
+    getline(cin, first->judul);
+
+    string judul = first->judul + ext;
+
+    cout << first->judul << endl;
+
     cout << " Di atas awan \n ";
     cout << " BBBBBBB\n";
     cout << "C";
     cout << "D";
     cout << 3 + 9 << endl;
-    PlaySound(TEXT("Mawar Jingga.wav"), NULL, SND_FILENAME | SND_SYNC);
+    PlaySoundA(judul.c_str(), NULL, SND_FILENAME | SND_SYNC);
     system("pause");
     return 0;
 }
