37aac9fb8472b120d91b02f157f1a37e6962b149
bariqjinan <jinanbariq50@gmail.com>
2023-12-05 22:47:07 +0700

    Menggabungkan kerangka dengan "Putar Lagu Apa?"

--- 1.cpp
+++ 1.cpp
@@ -18,23 +18,65 @@ int main()
 {
     pointer first = new lagu;
 
-    string ext = ".wav";
+    int pilih;
 
-    cout << "Masukkan Judul Lagu: " << endl;
-    getline(cin, first->judul);
+    do
+    {
+        cout << "|======================|" << endl;
+        cout << "| Program Pemutar Lagu |" << endl;
+        cout << "|======================|" << endl;
+        cout << "Pilihan Menu" << endl;
+        cout << "1. Tambah lagu" << endl;
+        cout << "2. Tambah playlist" << endl;
+        cout << "3. Putar lagu apa?" << endl;
+        cout << "4. Masukan Antrian" << endl;
+        cout << "5. Tampilkan lagu" << endl;
+        cout << "6. Hapus lagu pengguna" << endl;
+        cout << "7. Hapus Antrian lagu" << endl;
+        cout << "8. Stop lagu pengguna" << endl;
+        cout << "9. Delete Playlist" << endl;
+        cout << "10.Keluar Program" << endl;
+        cout << "Masukkan Pilihan : ";
+        cin >> pilih;
+        switch (pilih)
+        {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+        {
+            cin.ignore();
+            cout << "Masukkan Judul Lagu: " << endl;
+            getline(cin, first->judul);
+            string ext = ".wav";
 
-    cout << "asjdsa" << endl;
+            string Judul = first->judul + ext;
 
-    string judul = first->judul + ext;
-
-    cout << first->judul << endl;
-
-    cout << " Di atas awan \n ";
-    cout << " BBBBBBB\n";
-    cout << "C";
-    cout << "D";
-    cout << 3 + 9 << endl;
-    PlaySoundA(judul.c_str(), NULL, SND_FILENAME | SND_SYNC);
-    system("pause");
-    return 0;
+            PlaySoundA(Judul.c_str(), NULL, SND_FILENAME | SND_ASYNC);
+            break;
+        }
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            cout << "Program berhenti" << endl;
+            cout << "Terima Kasih" << endl;
+            break;
+        default:
+            cout << "Maaf pilihan Menu tidak tersedia" << endl;
+            break;
+        }
+        system("PAUSE");
+        system("cls");
+    } while (pilih != 10);
 }
