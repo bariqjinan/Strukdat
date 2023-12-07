18167f4a506dec3ab18909f63d93f1453077c446
bariqjinan <jinanbariq50@gmail.com>
2023-12-07 11:31:34 +0700

    Nambahin fungsi buat ngedownload lagu

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <winsock2.h>
 #include <windows.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <fstream>
+#include <curl/curl.h>
 #include <string>
 #include "MMSystem.h"
 #include <winnt.h>
@@ -11,14 +14,52 @@
 
 using namespace std;
 
+string loc = "Musik\\";
+string ext = ".wav";
 struct lagu
 {
     string judul;
     lagu *next;
+    lagu *prev;
 };
+void download(string ur, string judul)
+{
+    string url = ur;
+    string name = loc + judul + ext;
+    const char *convu = url.c_str();
+    if (S_OK == URLDownloadToFileA(NULL, url.c_str(), name.c_str(), 0, NULL))
+    {
+        cout << "Berhasil Ditambahkan" << endl;
+    }
+    else
+    {
+        cout << "Gagal" << endl;
+    }
+}
+
+typedef lagu *pointer_lagu;
+void insertLast(pointer_lagu &first, pointer_lagu &pBaru)
+{
+    pointer_lagu last;
+    if (first == NULL)
+    {
+        first = pBaru;
+    }
+    else
+    {
+        last = first;
+        while (last->next != NULL)
+        {
+            last = last->next;
+        }
+        last->next = pBaru;
+        pBaru->prev = last;
+    }
+}
 
 void readDirectory()
 {
+    // pointer_lagu first = NULL, pBaru;
     DIR *dir;
     struct dirent *entry;
     struct stat sb;
@@ -31,11 +72,6 @@ void readDirectory()
     int i = 0;
     while ((entry = readdir(dir)) != nullptr)
     {
-        // // Check if the entry is a file or directory
-        // if (stat(entry->d_name, &sb) == -1)
-        // {
-        //     perror("Failed to get file information");
-        //         }
 
         string file_name = entry->d_name;
 
@@ -43,32 +79,31 @@ void readDirectory()
         if (file_name.size() >= 4 &&
             file_name.substr(file_name.size() - 4) == ".wav")
         {
+            // pBaru->prev = NULL;
             file_name = file_name.substr(0, file_name.size() - 4);
             cout << i + 1 << ". " << file_name << endl;
+
+            // pBaru->judul = file_name;
+            // if (first == NULL)
+            // {
+            //     first = pBaru;
+            // }
+            // else
+            // {
+            //     insertLast(first, pBaru);
+            // }
+            // pBaru = pBaru->next;
             i++;
         }
-
-        // if (S_ISREG(sb.st_mode))
-        // {
-        //     cout << entry->d_name << " is a regular file." << endl;
-        // }
-        // else if (S_ISDIR(sb.st_mode))
-        // {
-        //     cout << entry->d_name << " is a directory." << endl;
-        // }
-        // else
-        // {
-        //     cout << entry->d_name << " is an unknown type." << endl;
-        // }
     }
-
     closedir(dir);
+    // pBaru->next = NULL;
 }
-typedef lagu *pointer;
 
 int main()
 {
-    pointer first = new lagu;
+
+    pointer_lagu first = new lagu, pBaru, pCari, pHapus;
 
     int pilih;
 
@@ -93,7 +128,18 @@ int main()
         switch (pilih)
         {
         case 1:
-            break;
+        {
+            cin.ignore();
+            string web, lagu;
+            cout << "Mau Lagu apa?";
+            getline(cin, lagu);
+            cout << lagu << endl;
+            cout << "Masukkin URL-nya";
+            getline(cin, web);
+            cout << web << endl;
+            download(web, lagu);
+        }
+        break;
         case 2:
             break;
         case 3:
@@ -101,8 +147,6 @@ int main()
             cin.ignore();
             cout << "Masukkan Judul Lagu: " << endl;
             getline(cin, first->judul);
-            string loc = "Musik\\";
-            string ext = ".wav";
 
             string Judul = loc + first->judul + ext;
             cout << Judul << endl;
@@ -112,9 +156,10 @@ int main()
         case 4:
             break;
         case 5:
-
+        {
             readDirectory();
-            break;
+        }
+        break;
         case 6:
             break;
         case 7:
